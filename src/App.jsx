a33759b59ddab5cfdd8db74ed2e844252d72e778
rvrutan <rutanroni@gmail.com>
2026-03-14 22:47:33 -0400

    create

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-orange-400 flex items-center justify-center">
+      <div className="text-center">
+        <p className="text-4xl text-base-content text-black">alanrutan.com</p>
+        <p className="text-lg mt-2 text-black">Coming Soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
