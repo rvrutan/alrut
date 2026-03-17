fd51afbd8b7a2c64e9658122fd6b732e75558e88
rvrutan <rutanroni@gmail.com>
2026-03-16 22:49:25 -0400

    adjust home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ function App() {
     <div className="min-h-screen bg-orange-400 flex items-center justify-center">
       <div className="text-center">
         <p className="text-4xl text-base-content text-black">alanrutan.com</p>
-        <p className="text-lg mt-2 text-black">Coming Soon</p>
       </div>
     </div>
   );
