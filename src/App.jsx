694ae3a9e1726599dbb02694f323dacb4896390f
rvrutan <rutanroni@gmail.com>
2026-06-22 01:01:09 -0400

    update color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <div className="min-h-screen bg-orange-400 flex items-center justify-center">
+    <div className="min-h-screen bg-yellow-600 flex items-center justify-center">
       <div className="text-center">
-        <p className="text-4xl text-base-content text-black">alanrutan.com</p>
+        <p className="text-6xl text-base-content text-blue-900">
+          <a href="https://alanrutan.com" target="_blank" rel="noopener noreferrer">
+            alanrutan.com
+          </a>
+        </p>
       </div>
     </div>
   );
