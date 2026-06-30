35edc07043cd6751adff7d49e8818b45cf9a5041
rvrutan <rutanroni@gmail.com>
2026-06-30 08:44:37 -0400

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 function App() {
   return (
-    <div className="min-h-screen bg-yellow-600 flex items-center justify-center">
+    <div className="min-h-screen bg-yellow-500 flex items-center justify-center">
       <div className="text-center">
         <p className="text-6xl text-base-content text-blue-900">
-          <a href="https://alanrutan.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://alanrutan.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             alanrutan.com
           </a>
         </p>
+        <div className="mt-12">
+          <button
+            className="btn btn-md btn-outline bg-blue-900 text-yellow-400 px-8 hover:scale-110 hover:shadow-lg transition cursor-pointer"
+            onClick={() => window.open("https://ronirutan.com", "_blank")}
+          >
+            Need a website?
+          </button>
+        </div>
       </div>
     </div>
   );
